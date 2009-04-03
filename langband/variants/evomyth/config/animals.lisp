afc0f198436b368c866ba58195c8d28c3fa16cb4
Stig <ses@vizrt.com>
2009-04-03 11:13:28 +0200

    Added sabretooth mouse

--- langband/variants/evomyth/config/animals.lisp
+++ langband/variants/evomyth/config/animals.lisp
@@ -20,12 +20,30 @@ Copyright (c) 2009 - Stig Erik Sandoe
   :hitpoints '(2 . 10)
   :armour 50
   :speed 120
-  ;;:abilities '(<bash-door> <open-door> <pick-up-item> (<random-mover> 1/4))
   :abilities '()
   :alertness 250
   :vision 15
   ;;:attacks '((<hit> :type <hurt> :damage (1 . 10)))
-  ;;:treasures '((<drop-chance> 9/10))
+  :strategies '(<avoid-carnivore> <avoid-omnivore> <avoid-player> <fight-if-cornered>)
+  :type '(<mammal>)
+  :diet '<herbivore>
+  :gender '<female>)
+
+(define-monster-kind "sabretooth-mouse" "Sabretooth Mouse"
+  :numeric-id  2001
+  :gfx-sym (tile-paint-value 16 24)
+  :desc "A small, but vicious beast that avoids people.  They are omnivores and can kill most small animals.  The meat can be quite tasty although a bit stringy.."
+  :text-sym (text-paint-value +term-white+ #\m)
+  :depth 0
+  :rarity 1
+  :power-lvl 2
+  :hitpoints '(2 . 10)
+  :armour 20
+  :speed 110
+  :abilities '()
+  :alertness 150
+  :vision 8
+  ;;:attacks '((<hit> :type <hurt> :damage (1 . 10)))
   :strategies '(<avoid-carnivore> <avoid-omnivore> <avoid-player> <fight-if-cornered>)
   :type '(<mammal>)
   :diet '<herbivore>
