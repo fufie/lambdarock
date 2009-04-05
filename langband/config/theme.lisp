960ba16ce0279106a367fefe930cf9a4f9fab544
Stig <ses@vizrt.com>
2009-04-05 21:55:34 +0200

    Hack to experiment with theming

--- langband/config/theme.lisp
+++ langband/config/theme.lisp
@@ -30,7 +30,7 @@
 
 (ui-theme "default-sdl"
        ;; which system does the theme apply to
-       :system "sdl"
+       :system "xsdl"
        :default-font "vga8x16.hex" ;; this one is also fallback if other fonts fail, must be safe!
        
        (big      :key +full-frame+
@@ -188,4 +188,122 @@
 		 :x 0 :y (var msg height)
 		 :width window.width
 		 :height (- window.height (var msg height) (var misc height)))
+       )
+
+(ui-theme "fancy-sdl"
+       ;; which system does the theme apply to
+       :system "sdl"
+       :default-font "vga8x16.hex" ;; this one is also fallback if other fonts fail, must be safe!
+       
+       (big      :key +full-frame+
+		 :x 0 :y 0
+		 :width window.width
+		 :height window.height
+		 :font "vga8x16.hex"
+		 ;;:font "lettergo.ttf"
+		 )
+       
+       ;; disabled
+       (charinfo :key +charinfo-frame+
+		 :x 0 :y 12
+		 :disabled? true
+		 ;;:disabled? false
+		 :width (* 12 (var charinfo tile-width)) ;; we need 13 columns
+		 :height (- (var msg y-offset) (var charinfo y-offset) (var infodisp height))
+		 :background "textures/bumpi.png"
+		 :font ("vga8x16.hex")
+		 ;;:font "lettergo.ttf"
+		 )
+       
+       (infodisp :key +infodisp-frame+
+		 :disabled? false
+		 ;;:x 0 :y (+ (var charinfo y-offset) (var charinfo height))
+		 :x 0
+		 :y (- window.height (* 2 gfxtiles.height))
+		 :height (* 2 gfxtiles.height)
+		 :width (* 3 gfxtiles.width)
+		 :font "vga8x16.hex"
+		 :tile-width gfxtiles.width
+		 :tile-height gfxtiles.height
+		 :background "textures/invbg2.png"
+		 :gfx-tiles? true
+		 )
+       
+       (gfxmap   :key +gfxmap-frame+
+		 :x 0 :y 0
+		 :width window.width
+		 :height (- window.height (* 3 gfxtiles.height))
+		 :tile-width gfxtiles.width
+		 :tile-height gfxtiles.height
+		 :gfx-tiles? true)
+
+       (asciimap :key +asciimap-frame+
+		 :x window.width :y 0
+		 :width window.width
+		 :height (- window.height 3)
+		 :font "vga8x16.hex"
+		 :gfx-tiles? false)
+
+       
+       (msg      :key +message-frame+
+		 :x 0 :y (- window.height (var msg height) (var dialogue height))
+		 :width (- window.width (var infodisp width))
+		 :height (* (var msg tile-height) 2) ;; how many rows?
+		 ;;:background "textures/bumpi.png"
+		 :background "textures/blue.png"
+		 ;;:background "textures/woodfloor.png"
+		 :font "vga8x16.hex"
+		 ;; other styles are normal and italic
+		 ;;:font #+win32 "vga8x16.hex" #-win32 ("lettergo.ttf" normal 14) 
+		 )
+       
+       (misc     :key +misc-frame+
+		 :x 0 :y (- window.height (+ (var inventory height) (var misc height)))
+		 :width (- window.width (var tiledfields width))
+		 :height (var misc tile-height)
+		 ;;:background "textures/bumpi.png"
+		 :background "textures/green.png"
+		 ;;:font #+win32 "vga8x16.hex" #-win32 "lettergo.ttf"
+		 :font "vga8x16.hex"
+		 :disabled? true
+		 )
+
+       (inventory :key +inv-frame+
+		  ;;:disabled? false
+		  :disabled? true
+		  :x 0 :y (- window.height (* gfxtiles.height 1))
+		  :width (- window.width (* 2 gfxtiles.width))
+		  :height (* gfxtiles.height 1)
+		  :background "textures/invbg2.png"
+		  :tile-width gfxtiles.width
+		  :tile-height gfxtiles.height
+		  :font ("vga8x16.hex")
+		  ;;:font "lettergo.ttf"
+		  :gfx-tiles? true)
+       
+       (dialogue :key +dialogue-frame+
+		 :x 0 :y 0
+		 :disabled? true
+		 :width window.width
+		 :height (- (var msg y-offset) (var dialogue y-offset))
+		 :font "vga8x16.hex"
+		 :background "textures/woodfloor.png"
+		 :gfx-tiles? false)
+
+       ;; shows poison/blind/etc status
+       (tiledfields :key +tiledfields-frame+
+		:disabled? true
+		;;:disabled? true
+		:x (- window.width (var tiledfields width))
+		:y 0
+		:width (* 2 gfxtiles.width)
+		:height (- window.height (var infodisp height))
+		;;:background "textures/invbg2.png"
+		:tile-width gfxtiles.width
+		:tile-height gfxtiles.height
+		:font ("vga8x16.hex")
+		:background 0 ;; in backgrounds file, only when wid/hgt is like gfxtiles 
+		;;:font "lettergo.ttf"
+		:gfx-tiles? true)
+       
        )
