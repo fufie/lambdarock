7d826f0d4de2c7a0ceb61ef54d2a27c23ea8fe1e
Stig <ses@vizrt.com>
2009-04-09 14:56:40 +0200

    Added more abilities

--- langband/variants/evomyth/config/abilities.lisp
+++ langband/variants/evomyth/config/abilities.lisp
@@ -2,7 +2,7 @@
 
 #|
 
-DESC: variants/evomyth/config/abillities.lisp
+DESC: variants/evomyth/config/abilities.lisp
 Copyright (c) 2009 - Stig Erik Sandoe
 
 |#
@@ -13,14 +13,30 @@ Copyright (c) 2009 - Stig Erik Sandoe
   :key '<nutrition>)  
 (define-racial-ability "sight" "Sight"
   :key '<sight>)
+(define-racial-ability "smell" "Smell"
+  :key '<smell>)
+(define-racial-ability "hearing" "Hearing"
+  :key '<hearing>)
+(define-racial-ability "mouth" "Mouth"
+  :key '<mouth>)
 (define-racial-ability "hands" "Hands"
   :key '<hands>)
+(define-racial-ability "feet" "Feet"
+  :key '<feet>)
+(define-racial-ability "muscle" "Muscle"
+  :key '<muscle>)
+(define-racial-ability "organs" "Internal Organs"
+  :key '<organs>)
 (define-racial-ability "skin" "Skin"
   :key '<skin>)
+(define-racial-ability "skeleton" "Skeleton"
+  :key '<skeleton>)
+(define-racial-ability "fur" "Fur"
+  :key '<fur>)
+(define-racial-ability "head" "Head"
+  :key '<head>)
 
-
-
-
+;;; === Nutrition ===
 (define-racial-ability "carnivore" "Carnivore"
   :type '<nutrition>
   :description "You're able to get nutrition from meat."
@@ -49,7 +65,8 @@ Copyright (c) 2009 - Stig Erik Sandoe
   :power-lvl 20
   :hidden t
   :levels nil)
- 
+
+;;; === Sight ===
 
 (define-racial-ability "detail-vision" "Detailed vision"
   :type '<sight>
@@ -80,3 +97,109 @@ Copyright (c) 2009 - Stig Erik Sandoe
   :power-lvl 30
   :levels 5)
 
+;;; === Smell ===
+
+(define-racial-ability "detail-smell" "Detailed smell"
+  :type '<smell>
+  :description "Detailed smell allows you to smell things over a longer distance, as well as identify more information about targets."
+  :key '<detailed-smell>
+  :power-lvl 5
+  :levels 5)
+
+;;; === Hearing ===
+
+(define-racial-ability "detail-hearing" "Detailed hearing"
+  :type '<hearing>
+  :description "Detailed vision allows you to spot details at a long distance and see e.g weak spots on enemies."
+  :key '<detailed-vision>
+  :power-lvl 5
+  :levels 5)
+
+;;; === Mouth ===
+
+(define-racial-ability "fangs" "Fangs"
+  :type '<mouth>
+  :description "Fangs are long pointed teeth used for biting and tearing flesh.  The damage can be severe and critical."
+  :key '<fangs>
+  :power-lvl 5
+  :levels 5)
+
+(define-racial-ability "poison-fangs" "Poisoned fangs"
+  :type '<mouth>
+  :description "Poisoned fangs allows a solid bite to also distribute poison, typically causing paralysation, crippling pain or even instant death."
+  :key '<poison-fangs>
+  :power-lvl 5
+  :levels 5)
+
+(define-racial-ability "sabretooth-fangs" "Sabretooth fangs"
+  :type '<mouth>
+  :description "Sabretooth fangs are the ultimate in fangs.  Razor-sharp, long and able to tear even the toughest skin, as well as penetrating deep causing internal damage."
+  :key '<sabretooth-fangs>
+  :power-lvl 5
+  :levels 5)
+
+;;; === Hands ===
+
+(define-racial-ability "claws" "Claws"
+  :type '<hands>
+  :description "Claws allow your unarmed hits damage more, and increases the chance of critical hits."
+  :key '<claws>
+  :power-lvl 5
+  :levels 5)
+
+;;; === Feet ===
+
+(define-racial-ability "speedy-feet" "Speedy feet"
+  :type '<feet>
+  :description "Speedy feet allows you to move faster."
+  :key '<speedy-feet>
+  :power-lvl 5
+  :levels 5)
+
+;;; === Muscle ===
+
+(define-racial-ability "strong-muscles" "Strong muscles"
+  :type '<muscle>
+  :description "Strong muscles allows you to carry more and hit harder."
+  :key '<strong-muscle>
+  :power-lvl 5
+  :levels 5)
+
+;;; === Internal Organs ===
+
+;;; === Skin ===
+
+(define-racial-ability "barkskin" "Barkskin"
+  :type '<skin>
+  :description "Barkskin makes your skin tough and robust as bark, making it harder to penetrate."
+  :key '<barkskin>
+  :power-lvl 5
+  :levels 5)
+
+;;; === Skeleton ===
+
+(define-racial-ability "strong-skeletong" "Strong skeleton"
+  :type '<skin>
+  :description "DESCRIPTION."
+  :key '<strong-skeleton>
+  :power-lvl 5
+  :levels 5)
+
+
+;; === Fur ===
+
+(define-racial-ability "wolf-fur" "Wolf fur"
+  :type '<fur>
+  :description "Wolf fur is quite resistant to both water and cold."
+  :key '<wolf-fur>
+  :power-lvl 5
+  :levels 5)
+
+;; === Head ===
+
+(define-racial-ability "horn" "Horn"
+  :type '<head>
+  :description "Horns can be quite useful when head-butting softer things."
+  :key '<horn>
+  :power-lvl 5
+  :levels 5)
