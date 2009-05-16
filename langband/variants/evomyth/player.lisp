1384a88477869bf81cfd35a443f169e55b9b11d9
Stig <ses@vizrt.com>
2009-05-16 14:50:48 +0200

    At least list out ability groups

--- langband/variants/evomyth/player.lisp
+++ langband/variants/evomyth/player.lisp
@@ -258,7 +258,6 @@ Modififes the passed player object THE-PLAYER.  This is a long function."
     (refresh-window +full-frame+)
 
     #||
-    
     ;; let's figure out the skill-basics
     (let* ((the-class (player.class player))
 	   (skills (class.skills the-class)))
@@ -276,9 +275,11 @@ Modififes the passed player object THE-PLAYER.  This is a long function."
 	      (when s-obj
 		(setf (gethash (evo/skill.slot s-obj) (player.skills player)) (second i)))))))
       )
-
-    (interactive-skillpoint-distribution variant player settings)
+        (interactive-skillpoint-distribution variant player settings)
     ||#
+
+    (interactive-ability-distribution variant player settings)
+    
     
     (clear-window +full-frame+)
     (refresh-window +full-frame+)
@@ -405,126 +406,23 @@ can be assigned later." :end-col (- right-col column-spacing))
     t))
 
 (defun interactive-ability-distribution (variant player settings)
-  ;;(let ((ability-groups (variant.ability-groups variant)))
+  (let ((ability-groups (variant.ability-groups variant))
+	(left-side (setting-lookup settings "skill-left-x"))
+	(right-col (setting-lookup settings "skill-right-x"))
+	(start-row (setting-lookup settings "skill-y")))
     
-  #||
-  (let* ((skill-table (variant.skills variant))
-	 (objs (make-array (length skill-table) :initial-element nil))
-	 (scores (make-array (length skill-table) :initial-element 0))
-	 (score-diffs (make-array (length skill-table) :initial-element 0))
-	 (points-to-allocate 20)
-	 (left-side (setting-lookup settings "skill-left-x"))
-	 (right-col (setting-lookup settings "skill-right-x"))
-	 (start-row (setting-lookup settings "skill-y"))
-	 (column-spacing (setting-lookup settings "skill-column-xpad"))
-	 )
+    (let ((groups (sort (loop for group-key being the hash-keys of ability-groups
+		       when (stringp group-key)
+			 collecting group-key)
+			#'string-lessp)))
 
-    (loop for x across skill-table
+    (loop for group in groups
+	  for i from 0
 	  do
-	  (when x
-	    (let ((val (gethash (evo/skill.slot x) (player.skills player))))
-	      (setf (aref scores (evo/skill.idx x)) val))))
-    
-    (flet ((display-skill (num colour)
-	     (let ((x (aref objs num)))
-	       (when x
-		 (put-coloured-str! +term-green+ (format nil "~2,'0d" (+ (aref scores num)
-									 (aref score-diffs num)))
-				    (second x) (third x))
-		 (put-coloured-str! colour (evo/skill.id (first x)) (+ 5 (second x)) (third x)))))
-	   
-	   (display-points-left (row)
-	     (put-coloured-str! +term-blue+ "Points left:" (1+ left-side) row)
-	     (put-coloured-str! +term-green+ (format nil "~2d" points-to-allocate) (+ 14 left-side) row)
-	     (put-coloured-str! +term-blue+ "===============" (1+ left-side) (1+ row)))
-	   
-	   (display-skill-desc (num row)
-	     ;; dumbo clear
-	     (dotimes (i 12)
-	       (put-coloured-str! +term-red+ "                             " left-side (+ i row)))
-	     (when-bind (sk (aref skill-table num))
-	       (put-coloured-str! +term-blue+ "Skill: " (1+ left-side) row)
-	       (put-coloured-str! +term-red+ (evo/skill.id sk) (+ 8 left-side) row)
-	       (print-text! left-side (+ 2 row) +term-umber+
-			    (evo/skill.desc sk) :end-col (- right-col column-spacing))
-	       )))
-	     
-      (put-coloured-str! +term-blue+ "Assigning skillpoints" (1+ left-side) start-row)
-      ;;(put-coloured-str! +term-blue+ "=====================" (1+ left-side) 2)
-
-      (print-text! left-side (+ 2 start-row) +term-umber+ 
-		   "Please assign skillpoints to the skills you wish to develop. 
-Add a point to a skill with the '+' key and use '-' if you change your mind.  Move 
-up and down the skill-list with the arrowkeys.  Hit ESC or 'Q' when 
-you're done.  Information about a skill will be shown below.  Leftover points 
-can be assigned later." :end-col (- right-col column-spacing))
-
-    
-      (loop for i from 0
-	    for x across skill-table
-	    when x do
-	    (progn
-	      (setf (aref objs i) (list x right-col (+ start-row i)))))
-
-
-      (dotimes (i (length objs))
-	(display-skill i +term-blue+))
-
-      (block award-points
-	(let ((current 0)
-	      (last nil)
-	      (max (1- (length objs))))
-	  (loop
-	   (display-skill current +term-l-red+)
-	   (display-points-left (+ start-row 20))
-	   (display-skill-desc current (+ start-row 23))
-	   
-	   (setf last current)
-	   (let ((key-input (read-one-character)))
-	     (case key-input
-	       (#\8 (decf current))
-	       (#\2 (incf current))
-	       ((#\+ #\6) (when (plusp points-to-allocate)
-		      (incf (aref score-diffs current))
-		      (decf points-to-allocate)))
-
-	       ((#\- #\4)(when (plusp (aref score-diffs current))
-		      (decf (aref score-diffs current))
-		      (incf points-to-allocate)))
-	     
-	       (#\Escape (return-from award-points nil))
-	       (#\Q (return-from award-points nil))
-	       )
-
-	     (when (minusp current)
-	       (setf current max))
-	     (when (> current max)
-	       (setf current 0))
-
-	     ;;(warn "current is ~s, last is ~s, points left ~s" current last points-to-allocate)
-
-	     ;; change colour if there has been a change
-	     (if (plusp (aref score-diffs last))
-		 (display-skill last +term-red+)
-		 (display-skill last +term-blue+))
-	     )
-	   )))
-      )
-
-    ;; add diffs here
-    (loop for i from 0
-	  for x across score-diffs
-	  for s-o = (aref skill-table i)
-	  do
-	  (when s-o
-	    (incf (gethash (evo/skill.slot s-o) (player.skills player))
-		  (aref score-diffs i))))
-    ;;(warn "player.skills ~s" (player.skills player))
+       (put-coloured-str! +term-blue+ group (+ 5 10) (+ 5 i))))
 
     (pause-last-line!)
-||#    
-    t)
-
+    t))
 
 
 (defmethod on-new-player ((variant evomyth) (player player))
@@ -540,6 +438,6 @@ can be assigned later." :end-col (- right-col column-spacing))
 
   player)
 
-(defmethod roll-hitpoints-for-new-level ((variant evomyth) (player player))
-  "Hitpoints are 1d10 + evo/5 for each lvl."
-  (+ (randint 10) (round-/ (aref (player.active-stats player) 2) 5)))
+  (defmethod roll-hitpoints-for-new-level ((variant evomyth) (player player))
+    "Hitpoints are 1d10 + evo/5 for each lvl."
+    (+ (randint 10) (round-/ (aref (player.active-stats player) 2) 5)))
